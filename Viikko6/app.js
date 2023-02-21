a6bd6d9a61245562248395abf93291d62e0cf231
JuusoKorpinen <122262588+JuusoKorpinen@users.noreply.github.com>
2023-02-21 15:07:08 +0200

    Viikon 6 tehtävät

--- Viikko6/app.js
+++ Viikko6/app.js
@@ -14,43 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/example',
-    function(request,response){
-        response.send('Mina olen esimerkki');
-        console.log('Mina olen esimerkki');
-    }
-);
-app.use('/example', //Kutsuu etunimi middlewaren
-    function(req,res,next){
-        console.log('Etunimi middleware kutsuttiin');
-        next();
-    }
-); 
-app.use('/example2', //Kutsuu etunimi sukunimi middlewaren
-    function(req,res,next){
-        console.log('Etunimi ja Sukunimi middleware kutsuttiin');
-        next();
-    }
-); 
-
-app.get('/example/:name', //Tulostaa "Terve" ja annetun nimen
-    function(request,response){
-        response.send('Terve '+request.params.name);
-    }
-);
-
-app.get('/example2/:firstName/:lastName', //Tulostaa "Terve" ja annetun etu- ja sukunimen
-    function(request, response){
-        response.send('Terve '+request.params.firstName+" "+request.params.lastName);
-    }
-);
-
-app.post('/', //Tulostaa takaisin antaman datan
-    function(request,response){
-        response.send(request.body);
-        console.log(request.body);
-    }
-);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
